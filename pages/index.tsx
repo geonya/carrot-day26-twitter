e5d0594b019951deeb7e16e4d71cb76a3a64f373
geonya <geony8410@gmail.com>
2022-07-15 16:21:27 +0900

    SSR SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { Tweet } from '@prisma/client';
+import { NextPage } from 'next';
+import useSWR, { SWRConfig } from 'swr';
+import client from '../lib/server/client';
 
-export default () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
+interface TweetResponse {
+  ok: boolean;
+  tweets: Tweet[];
+}
+
+const Main: NextPage = () => {
+  const { data } = useSWR<TweetResponse>('/api/tweets');
+  console.log(data);
+  return <div>Main</div>;
+};
+// SSR + SWR
+const Page: NextPage<{ tweets: Tweet[] }> = ({ tweets }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          '/api/tweets': {
+            ok: true,
+            tweets,
+          },
+        },
+      }}
+    >
+      <Main />
+    </SWRConfig>
+  );
+};
+
+export async function getServerSideProps() {
+  const tweets = await client.tweet.findMany({});
+  console.log('tweets', tweets);
+  return {
+    props: {
+      tweets: [],
+    },
+  };
+}
+
+export default Page;
